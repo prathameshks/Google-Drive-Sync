cab62559913783a74adfb921c50d56cadf1f5b3b
Prathamesh Sable <82529994+prathameshks@users.noreply.github.com>
2024-07-03 09:49:54 +0530

    git ignore for testing

---
+++ test.py
@@ -0,0 +1,13 @@
+# from utility.drive_list import get_drive_file_list
+# from utility.local_list import get_local_file_list
+
+# local_files = get_local_file_list(r"D:\Coding\Google-Drive-Sync\test")
+# drive_files = get_drive_file_list(r"1t2GDmtyZsa4X_a3tn23UvLR4dcGurba_")
+
+# print(local_files)
+# print(drive_files)
+
+a = {1,2,3,4}
+b = {2,4,6,8}
+
+print(a-b)
